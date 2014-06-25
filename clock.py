02bb2679065d668be5066bdf4e7e1f3730c1e2dd
SCOTPAUL <paul.cowie@ntlworld.com>
2014-06-25 21:50:23 +0100

    Draws lines at the position of each hour

--- clock.py
+++ clock.py
@@ -19,11 +19,44 @@ class clockOuter(object):
         self.fill = fill
         self.width = width
         self.radius = radius
+        self.centre = centre
 
 
-    def draw(self):
+    def drawOuterRing(self):
         self.outer = self.canvas.create_oval(self.coords, outline = self.colour, fill = self.fill, width = self.width)
         self.canvas.tag_lower(self.outer)
+
+    def drawHourLines(self, num = 12, length = 20, width = 5.0):
+        angle = 0.0
+        angleStep = math.radians(360.0/num)
+
+        for lines in range(num):
+            startX = self.centre[0] + self.radius*math.sin(angle)
+            startY = self.centre[1] - self.radius*math.cos(angle)
+            endX = startX - length*math.sin(angle)
+            endY = startY + length*math.cos(angle)
+            lineCoords = (startX, startY, endX, endY)
+            print lineCoords
+            self.canvas.create_line(lineCoords, fill = self.colour, width = width)
+            angle += angleStep
+
+
+
+
+
+class clockCentre(object):
+
+    def __init__(self, canvas, radius, centre, colour = "#000000", fill = "#000000", width = 0.0):
+        self.coords = (centre[0]-radius, centre[1]-radius, centre[0]+radius, centre[1]+radius)
+        self.canvas = canvas
+        self.colour = colour
+        self.fill = fill
+        self.width = width
+        self.radius = radius
+
+    def draw(self):
+        self.shape = self.canvas.create_oval(self.coords, outline = self.colour, fill = self.fill, width = self.width)
+        self.canvas.tag_raise(self.shape)
 
 
 class hand(object):
@@ -41,7 +74,7 @@ class hand(object):
         self.cx = centre[0]
         self.cy = centre[1]
         self.px = centre[0] + self.length*math.sin(self.angle)
-        self.py = centre[0] - self.length*math.cos(self.angle)
+        self.py = centre[1] - self.length*math.cos(self.angle)
         self.coords = (self.cx, self.cy, self.px, self.py)
         self.shape = self.canvas.create_line(self.coords, fill = self.colour, width = self.width)
 
--- main.py
+++ main.py
@@ -18,14 +18,20 @@ clockFace = clock.clockOuter(win, CANWIDTH/2.0 - 10, (CANWIDTH/2.0, CANHEIGHT/2.
 secondHand = clock.hand(root, "second", win, clockFace.radius- (1/4.0)*clockFace.radius, (CANWIDTH/2.0, CANHEIGHT/2.0), width = 5.0, colour = "#FF0000")
 minuteHand = clock.hand(root, "minute", win, clockFace.radius- (1/3.0)*clockFace.radius, (CANWIDTH/2.0, CANHEIGHT/2.0), width = 8.0, colour = "#000000")
 hourHand = clock.hand(root, "hour", win, clockFace.radius- (1/2.0)*clockFace.radius, (CANWIDTH/2.0, CANHEIGHT/2.0), width = 10.0, colour = "#000000")
+centreRing = clock.clockCentre(win, 8, (CANWIDTH/2.0, CANHEIGHT/2.0))
 
-clockFace.draw()
+clockFace.drawOuterRing()
+clockFace.drawHourLines()
+
 secondHand.update()
 minuteHand.update()
 hourHand.update()
+
 secondHand.draw()
 minuteHand.draw()
 hourHand.draw()
+
+centreRing.draw()
 
 
 root.mainloop()
