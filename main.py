fea175dd3bc83f6c4d75a113538a1ab5a20cb37d
SCOTPAUL <paul.cowie@ntlworld.com>
2014-06-25 23:14:31 +0100

    Add configuration file for resolution

--- main.py
+++ main.py
@@ -2,17 +2,15 @@
 
 from Tkinter import *
 import clock
+from config import *
 
-
-CANWIDTH = 500
-CANHEIGHT = 500
 
 root = Tk()
 root.title("Clock")
 root.resizable(0,0)
 
 win = Canvas(root, width = CANWIDTH, height = CANHEIGHT)
-win.grid(ipadx = 10, ipady = 10)  #Places canvas on screen
+win.grid()  #Places canvas on screen
 
 clockFace = clock.clockOuter(win, CANWIDTH/2.0 - 10, (CANWIDTH/2.0, CANHEIGHT/2.0), width = 5.0)
 secondHand = clock.hand(root, "second", win, clockFace.radius- (1/4.0)*clockFace.radius, (CANWIDTH/2.0, CANHEIGHT/2.0), width = 5.0, colour = "#FF0000")
